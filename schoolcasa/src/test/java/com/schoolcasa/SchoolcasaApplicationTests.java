c7f8358099a066882243c4457435679f69ded746
manishayacham <manisha.yacham16@gmail.com>
2020-12-15 12:51:54 -0800

    Initial backend folders

---
+++ schoolcasa/src/test/java/com/schoolcasa/SchoolcasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolcasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolcasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
