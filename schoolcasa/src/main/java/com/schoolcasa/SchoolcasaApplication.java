86eec33619786bdad5f3787cb1b8348ca819cdf1
jrangu <55324369+jrangu@users.noreply.github.com>
2020-12-16 15:52:05 -0800

    Added model for ApartmentListing. Changed application.properties and pom.xml.

--- schoolcasa/src/main/java/com/schoolcasa/SchoolcasaApplication.java
+++ schoolcasa/src/main/java/com/schoolcasa/SchoolcasaApplication.java
@@ -2,8 +2,10 @@ package com.schoolcasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.schoolcasa.model")
 public class SchoolcasaApplication {
 
 	public static void main(String[] args) {
---
+++ schoolcasa/src/main/java/com/schoolcasa/model/ApartmentListing.java
@@ -0,0 +1,94 @@
+package com.schoolcasa.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class ApartmentListing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int bedrooms;
+    private int bathrooms;
+    private double rent;
+    private String address;
+    private String description;
+    @Column(columnDefinition = "tinyint(1) default 0")
+    private boolean isParkingAvailable;
+    @Column(columnDefinition = "tinyint(1) default 0")
+    private boolean isDeleted;
+    private String imageURL;
+    
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
+    public boolean isParkingAvailable() {
+        return isParkingAvailable;
+    }
+
+    public void setParkingAvailable(boolean parkingAvailable) {
+        isParkingAvailable = parkingAvailable;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(int bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public double getRent() {
+        return rent;
+    }
+
+    public void setRent(double rent) {
+        this.rent = rent;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
